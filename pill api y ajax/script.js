42afcbededf08122f7259521fc6c61cc56eb35b5
josemari242 <josemari94@gmail.com>
2024-01-11 15:04:09 +0100

    step 3

--- pill api y ajax/script.js
+++ pill api y ajax/script.js
@@ -7,7 +7,21 @@ document.addEventListener('DOMContentLoaded', function () {
             displayPosts(posts);
         })
         .catch(error => console.error('Error fetching data:', error));
+
+   
+        fetch('http://localhost:3000/users')
+        .then(response => response.json())
+        .then(users => {
+            // Llamada a la función para mostrar los users
+            displayUsers(users);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+
+
 });
+
+
+
 
 function displayPosts(posts) {
     const postList = document.getElementById('postList');
@@ -29,11 +43,45 @@ function displayPostDetails(post) {
     // Actualizar contenido del modal con detalles del post
     document.getElementById('postTitle').textContent = post.title;
     document.getElementById('postBody').textContent = post.body;
+   
+    
 
-    // Mostrar el modal
-    const postModal = new bootstrap.Modal(document.getElementById('postModal'));
-    postModal.show();
+// Mostrar el modal
+const postModal = new bootstrap.Modal(document.getElementById('postModal'));
+postModal.show();
 }
+   
+// USERS
+
+function displayUsers(users) {
+    const postUsers = document.getElementById('postUsers');
+
+    // Limpiar el contenedor antes de agregar nuevos elementos
+    postUsers.innerHTML = '';
+
+    // Crear elementos para cada post y agregarlos al contenedor
+    users.forEach(users => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-group-item', 'cursor-pointer');
+        listItem.textContent = users.username;
+        listItem.addEventListener('click', () => displayUsersDetails(users));
+        postUsers.appendChild(listItem);
+    });
+}
+
+function displayUsersDetails(users) {
+    // Actualizar contenido del modal con detalles del post
+    document.getElementById('postUsername').textContent = users.username;
+    document.getElementById('postEmail').textContent = users.email;
+    //Mostrar el modal
+const postModal = new bootstrap.Modal(document.getElementById('postModal'));
+postModal.show();
+    
+}
+
+
+
+
 
 
 
